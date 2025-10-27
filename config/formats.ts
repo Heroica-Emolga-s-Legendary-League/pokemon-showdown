e904f5a3f5aec29407a3f95081a50b9181f124be
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2025-10-26 17:35:17 -0700

    Allowing Soul-Heart in the format

--- config/formats.ts
+++ config/formats.ts
@@ -564,6 +564,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		searchShow: false,
 		teraPreviewDefault: true,
 		ruleset: ['Standard Draft', '+Unobtainable', '+Past', 'Min Source Gen = 1', '+LGPE'],
+		unbanlist: ['Soul-Heart'],
 	},
 	{
 		name: "[Gen 9] NatDex 6v6 Doubles Draft",
