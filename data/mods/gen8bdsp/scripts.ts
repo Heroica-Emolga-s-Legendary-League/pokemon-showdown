b863293a2651f4f94eeca6adc5e01f3b6e2f9043
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2025-11-29 17:26:23 -0800

    Testing something

--- data/mods/gen8bdsp/scripts.ts
+++ data/mods/gen8bdsp/scripts.ts
@@ -11,17 +11,5 @@ export const Scripts: ModdedBattleScriptsData = {
 		for (const id of bdspPokedex) {
 			this.modData('Pokedex', id).isNonstandard = 'Past';
 		}
-
-		const bdspLearnsets = Object.keys(Learnsets);
-		for (const id of bdspLearnsets) {
-			const learnset = Learnsets[id as keyof typeof Learnsets].learnset;
-			for (const moveid in learnset) {
-				const move = learnset[moveid as keyof typeof learnset];
-
-				if (!move.includes('8M')) {
-					this.modData('Learnsets', id).learnset[moveid]!.push('8M');
-				}
-			}
-		}
 	}
 };
