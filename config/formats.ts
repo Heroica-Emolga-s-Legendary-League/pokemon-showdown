bbb580e5c0b6d3a9f4cb6e05a7c6e3df75cddf9e
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2025-11-29 16:10:52 -0800

    Updating the format

--- config/formats.ts
+++ config/formats.ts
@@ -586,7 +586,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		name: "[Gen 8 BDSP] Draft",
 		mod: 'gen8bdsp',
 		searchShow: false,
-		ruleset: ['Standard Draft', 'Dynamax Clause'],
+		ruleset: ['Standard Draft', 'Dynamax Clause', '+Past'],
 	},
 	{
 		name: "[Gen 8 BDSP] NatDex Draft",
