9436df08b287e95e729557193e4479b494197f0e
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2025-11-29 17:06:48 -0800

    Adding a init script that addins the Nonstandard Past to mons that don't have it.

--- data/mods/gen8bdsp/scripts.ts
+++ data/mods/gen8bdsp/scripts.ts
@@ -1,5 +1,13 @@
 import { Moves } from './moves';
+import { Pokedex } from './pokedex';
 export const Scripts: ModdedBattleScriptsData = {
 	gen: 9,
 	inherit: 'gen9',
+	init() {
+		const bdspPokedex = Object.keys(Pokedex).filter(mon => Pokedex[mon as keyof typeof Pokedex].isNonstandard === undefined);
+
+		for (const id of bdspPokedex) {
+			this.modData('Pokedex', id).isNonstandard = 'Past';
+		}
+	}
 };
