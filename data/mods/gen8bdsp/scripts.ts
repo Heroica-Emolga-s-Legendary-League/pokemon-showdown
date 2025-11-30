b97929aad4cfa2fb318cd099b2c6100b4f1f7f12
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2025-11-29 18:44:48 -0800

    test

--- data/mods/gen8bdsp/scripts.ts
+++ data/mods/gen8bdsp/scripts.ts
@@ -9,6 +9,7 @@ export const Scripts: ModdedBattleScriptsData = {
 		const keys = Object.keys(FormatsData).filter(k => FormatsData[k as keyof typeof FormatsData].isNonstandard === undefined || FormatsData[k as keyof typeof FormatsData].isNonstandard === 'Future');
 		for (const k of keys) {
 			this.modData('FormatsData', k).isNonstandard = 'Past';
+			this.modData('Pokedex', k).gen = 8;
 		}
 
 		const items = Object.keys(Items).filter(i => Items[i as keyof typeof Items].isNonstandard === undefined || Items[i as keyof typeof Items].isNonstandard === 'Future');
