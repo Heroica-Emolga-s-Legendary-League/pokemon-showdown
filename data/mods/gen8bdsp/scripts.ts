504386aa0a1ab5e27f4b7c952c72d4e35b6fc739
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2025-11-29 16:03:57 -0800

    Adjusting the script to be gen 9

--- data/mods/gen8bdsp/scripts.ts
+++ data/mods/gen8bdsp/scripts.ts
@@ -1,12 +1,5 @@
 import { Moves } from './moves';
 export const Scripts: ModdedBattleScriptsData = {
-	gen: 8,
+	gen: 9,
 	inherit: 'gen9',
-	side: {
-		inherit: true,
-		canDynamaxNow() {
-			// Dynamaxing is not in BDSP
-			return false;
-		},
-	},
 };
