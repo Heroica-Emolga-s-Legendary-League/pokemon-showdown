74925dbd67973bcc5df19637772c510e24fc7461
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2025-11-29 17:19:03 -0800

    Should fix everything

--- data/mods/gen8bdsp/scripts.ts
+++ data/mods/gen8bdsp/scripts.ts
@@ -1,5 +1,7 @@
 import { Moves } from './moves';
 import { Pokedex } from './pokedex';
+import { Learnsets } from './learnsets';
+
 export const Scripts: ModdedBattleScriptsData = {
 	gen: 9,
 	inherit: 'gen9',
@@ -8,6 +10,18 @@ export const Scripts: ModdedBattleScriptsData = {
 
 		for (const id of bdspPokedex) {
 			this.modData('Pokedex', id).isNonstandard = 'Past';
+		}
+
+		const bdspLearnsets = Object.keys(Learnsets);
+		for (const id of bdspLearnsets) {
+			const learnset = Learnsets[id as keyof typeof Learnsets].learnset;
+			for (const moveid in learnset) {
+				const move = learnset[moveid as keyof typeof learnset];
+
+				if (!move.includes('8M')) {
+					this.modData('Learnsets', id).learnset[moveid]!.push('8M');
+				}
+			}
 		}
 	}
 };
