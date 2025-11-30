e8531f6be2ec9afedabbae71bb7d1b1945d16a8a
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2025-11-29 19:16:50 -0800

    test

--- data/mods/gen8bdsp/scripts.ts
+++ data/mods/gen8bdsp/scripts.ts
@@ -5,7 +5,7 @@ import { Abilities } from './abilities';
 
 export const Scripts: ModdedBattleScriptsData = {
 	gen: 9,
-	inherit: 'gen9',
+	inherit: 'gen8',
 	init() {
 		const keys = Object.keys(FormatsData).filter(k => FormatsData[k as keyof typeof FormatsData].isNonstandard === undefined || FormatsData[k as keyof typeof FormatsData].isNonstandard === 'Future');
 		for (const k of keys) {
